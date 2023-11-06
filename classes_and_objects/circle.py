86df937694693887e1dad0587961b94b630bc8b7
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-06 15:02:38 +0200

    add

---
+++ classes_and_objects/circle.py
@@ -0,0 +1,19 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        self.pi = 3.14
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_area(self):
+        return self.radius * self.radius * self.pi
+
+    def get_circumference(self):
+        return self.radius * 2 * self.pi
+
+
+circle = Circle(15)
+circle.set_radius(11)
+print(circle.get_area())
+print(circle.get_circumference())
