b96b8f941743a8f00d05084d6d91558740b3b4a1
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-09 17:14:41 +0200

    add

---
+++ inheritance/hierarchical_inheritance/project/main.py
@@ -0,0 +1,10 @@
+from project.animal import Animal
+from project.dog import Dog
+from project.cat import Cat
+
+d = Dog()
+c = Cat()
+print(d.eat())
+print(c.eat())
+print(d.bark())
+print(c.meow())
