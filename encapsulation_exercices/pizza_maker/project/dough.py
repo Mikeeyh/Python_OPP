79485ed70ce55268e2a6cb38749c7eeb2a75273b
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-13 11:25:59 +0200

    add

--- encapsulation_exercices/pizza_maker/project/dough.py
+++ encapsulation_exercices/pizza_maker/project/dough.py
@@ -4,12 +4,32 @@ class Dough:
         self.baking_technique = baking_technique
         self.weight = weight
 
-        if self.flour_type == "":
+    @property
+    def flour_type(self):
+        return self.__flour_type
+
+    @flour_type.setter
+    def flour_type(self, value):
+        if value == "":
             raise ValueError("The flour type cannot be an empty string")
+        self.__flour_type = value
 
-        if self.baking_technique == "":
+    @property
+    def baking_technique(self):
+        return self.__baking_technique
+
+    @baking_technique.setter
+    def baking_technique(self, value):
+        if value == "":
             raise ValueError("The baking technique cannot be an empty string")
+        self.__baking_technique = value
 
-        if self.weight <= 0:
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, value):
+        if value <= 0:
             raise ValueError("The weight cannot be less or equal to zero")
-
+        self.__weight = value
