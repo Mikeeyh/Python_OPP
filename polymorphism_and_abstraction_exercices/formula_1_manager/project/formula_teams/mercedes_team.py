3caa05f2f696dd6e51ebaf74a8b9ee30e8be709c
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-17 12:58:12 +0200

    add

--- polymorphism_and_abstraction_exercices/formula_1_manager/project/formula_teams/mercedes_team.py
+++ polymorphism_and_abstraction_exercices/formula_1_manager/project/formula_teams/mercedes_team.py
@@ -4,7 +4,7 @@ from project.formula_teams.formula_team import FormulaTeam
 class MercedesTeam(FormulaTeam):
     @staticmethod
     def get_team_data():
-        expenses = 250000
+        expenses = 200000
         sponsors = {"Petronas": {1: 1000000, 3: 500000},
                     "TeamViewer": {5: 100000, 7: 50000}}
         return expenses, sponsors
