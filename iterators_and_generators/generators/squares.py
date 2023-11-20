c112f4acfea820242117dfca6668b2b0c3594cec
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-21 00:28:40 +0200

    add

---
+++ iterators_and_generators/generators/squares.py
@@ -0,0 +1,25 @@
+def squares(n):
+    current = 1
+    while current <= n:
+        yield current ** 2
+        current += 1
+
+
+print(list(squares(5)))
+
+# Using iterator:
+
+# class squares:
+#     def __init__(self, n: int):
+#         self.n = n
+#         self.start_index = 1
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.start_index > self.n:
+#             raise StopIteration()
+#         result = self.start_index ** 2
+#         self.start_index += 1
+#         return result
