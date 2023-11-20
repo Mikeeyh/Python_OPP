24147868c3f6cce59773a1616adbab5cdb176542
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-20 15:04:50 +0200

    add

--- iterators_and_generators/vowels.py
+++ iterators_and_generators/vowels.py
@@ -20,6 +20,28 @@ class vowels:
                 return self.txt[index]
 
 
+# OR:
+
+
+class vowels:
+    def __init__(self, txt):
+        self.txt = txt
+        vowels_letters = "aeiou"
+        self.found_vowels = [char for char in self.txt if char.lower() in vowels_letters]
+        self.start_index = 0
+        self.end_index = len(self.found_vowels) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start_index > self.end_index:
+            raise StopIteration()
+        index = self.start_index
+        self.start_index += 1
+        return self.found_vowels[index]
+
+
 my_string = vowels('Abcedifuty0o')
 for char in my_string:
     print(char)
