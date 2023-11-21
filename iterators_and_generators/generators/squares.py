2c1121fea348a35098dac181e1e9abfb98fb664b
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-21 11:06:19 +0200

    add

--- iterators_and_generators/generators/squares.py
+++ iterators_and_generators/generators/squares.py
@@ -7,6 +7,14 @@ def squares(n):
 
 print(list(squares(5)))
 
+
+def squares(n):
+    for i in range(1, n + 1):
+        yield i ** 2
+
+
+print(list(squares(5)))
+
 # Using iterator:
 
 # class squares:
