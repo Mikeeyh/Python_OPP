ed90ac87755088cc6711bf2d027c1362a8aa6717
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-23 20:49:55 +0200

    add

--- decorators_exercises/example.py
+++ decorators_exercises/example.py
@@ -1,5 +1,10 @@
+from functools import wraps
+
+
 def t_decorator(func):
+    # @wraps(func)
     def wrapper(*arg):
+        """ documentations from wrapper """
         print("Starting")
         func()
         print("End")
@@ -8,7 +13,15 @@ def t_decorator(func):
 
 @t_decorator
 def t():
+    """ function documentations """
     print("Test")
 
 
 t()
+print(t.__name__)  # returns the name of the function 'wrapper'
+print(t.__doc__) # returns the documentations in wrapper function if there isn't any docs, returns 'None'
+
+"""
+If we add @wraps(func) we will receive the functions name 't' 
+and functions 't' documentations if any
+"""
