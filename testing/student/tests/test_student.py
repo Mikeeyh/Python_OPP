8b62f855efdf513179791aee5f1dccb8f9a96cb6
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-11-28 13:21:55 +0200

    add

--- testing/student/tests/test_student.py
+++ testing/student/tests/test_student.py
@@ -3,48 +3,67 @@ from project.student import Student
 
 
 class TestStudent(TestCase):
-    name = "Mike"
-    courses = {}
-
     def setUp(self):
-        self.student = Student(self.name, self.courses)
+        self.student_1 = Student("Student 1", {"Python": ["n1", "n2", "n3"], "JS": ["n1", "n2"]})
+        self.student_2 = Student("Student 2")
 
-    def test_correct_initialization(self):
-        self.assertEqual(self.name, self.student.name)
-        self.assertEqual(self.courses, self.student.courses)
+    def test_correct_initialization_with_courses(self):
+        self.assertEqual("Student 1", self.student_1.name)
+        self.assertEqual({"Python": ["n1", "n2", "n3"], "JS": ["n1", "n2"]}, self.student_1.courses)
 
-    def test_attributes_types(self):
-        self.assertIsInstance(self.student.name, str)
-        self.assertIsInstance(self.courses, dict)
+    def test_correct_initialization_without_courses(self):
+        self.assertEqual("Student 2", self.student_2.name)
+        self.assertEqual({}, self.student_2.courses)
+
+    def test_enroll_existing_courses(self):
+        result = self.student_1.enroll("Python", ["n4", "n5"], add_course_notes="N")
+
+        self.assertEqual("Course already added. Notes have been updated.", result)
+        self.assertEqual({"Python": ["n1", "n2", "n3", "n4", "n5"], "JS": ["n1", "n2"]}, self.student_1.courses)
+
+    def test_enroll_not_existing_course_with_y(self):
+        result = self.student_1.enroll("C#", ["n1", "n2"], add_course_notes="Y")
+
+        self.assertTrue("C#" in self.student_1.courses)
+        self.assertEqual("Course and course notes have been added.", result)
+        self.assertEqual(["n1", "n2"], self.student_1.courses["C#"])
+
+    def test_enroll_not_existing_course_with_empty_str(self):
+        result = self.student_1.enroll("C#", ["n1", "n2"], add_course_notes="")
+
+        self.assertTrue("C#" in self.student_1.courses)
+        self.assertEqual("Course and course notes have been added.", result)
+        self.assertEqual(["n1", "n2"], self.student_1.courses["C#"])
+
+    def test_enroll_not_existing_course_without_adding_notes(self):
+        result = self.student_1.enroll("C#", ["n1", "n2"], add_course_notes="N")
+
+        self.assertTrue("C#" in self.student_1.courses)
+        self.assertEqual("Course has been added.", result)
+        self.assertEqual([], self.student_1.courses["C#"])
+
+    def test_add_notes_to_existing_course(self):
+        self.student_2.enroll("C#", ["n1", "n2"])
+        result = self.student_2.add_notes("C#", "n3")
+
+        self.assertEqual("Notes have been updated", result)
+        self.assertEqual(["n1", "n2", "n3"], self.student_2.courses["C#"])
+
+    def test_add_notes_to_not_existing_course(self):
+        with self.assertRaises(Exception) as ex:
+            self.student_2.add_notes("C#", "n3")
+        self.assertEqual("Cannot add notes. Course not found.", str(ex.exception))
+
+    def test_leave_existing_course(self):
+        result = self.student_1.leave_course("Python")
+        self.assertEqual("Course has been removed", result)
+        self.assertNotIn("Python", self.student_1.courses)
+
+    def test_leave_not_existing_course(self):
+        with self.assertRaises(Exception) as ex:
+            self.student_1.leave_course("C#")
+        self.assertEqual("Cannot remove course. Course not found.", str(ex.exception))
 
 
-# class Student:
-#     def __init__(self, name: str, courses=None):
-#         if courses is None:
-#             courses = {}
-#         self.name = name
-#         self.courses = courses  # {course_name: [notes]}
-#
-#     def enroll(self, course_name: str, notes, add_course_notes: str = ""):
-#         if course_name in self.courses.keys():
-#             [self.courses[course_name].append(x) for x in notes]
-#             return "Course already added. Notes have been updated."
-#
-#         if add_course_notes == "Y" or add_course_notes == "":
-#             self.courses[course_name] = notes
-#             return "Course and course notes have been added."
-#
-#         self.courses[course_name] = []
-#         return "Course has been added."
-#
-#     def add_notes(self, course_name, notes):
-#         if course_name in self.courses.keys():
-#             self.courses[course_name].append(notes)
-#             return "Notes have been updated"
-#         raise Exception("Cannot add notes. Course not found.")
-#
-#     def leave_course(self, course_name):
-#         if course_name in self.courses.keys():
-#             self.courses.pop(course_name)
-#             return "Course has been removed"
-#         raise Exception("Cannot remove course. Course not found.")
+if __name__ == "__main__":
+    main()
--- youtube_test.py
+++ youtube_test.py
@@ -1,1 +1,1 @@
-print(list(filter(lambda x: x % 2 != 0, [1, 2, 3, 4, 5, 6, 7])))
+print([x**2 for x in range(10) if x % 2 == 0])
